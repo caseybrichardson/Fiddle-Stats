13f14d75deb5579a44313f785608e7a873775a23
Casey Richardson <caseybrichardson@icloud.com>
2014-12-07 17:52:58 -0600

    Fixed an issue where typing in a summoner name that contained a space crashed the app.

--- Fiddle Stats/Summoner+APIMethods.m
+++ Fiddle Stats/Summoner+APIMethods.m
@@ -81,9 +81,9 @@
 
 + (void)summonerInformationFor:(NSString *)summonerName region:(NSString *)region withBlock:(void (^)(Summoner *, NSError *))block {
     NSDictionary *requestParams = @{@"api_key": @"8ad21685-9e9f-4c18-9e72-30b8d598fce9"};
-    NSString *url = [NSString stringWithFormat:@"/api/lol/%@/v1.4/summoner/by-name/%@", region, summonerName];
     
-    summonerName = [summonerName stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@" "]];
+    NSString *sanitizedName = [summonerName stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLPathAllowedCharacterSet]];
+    NSString *url = [NSString stringWithFormat:@"/api/lol/%@/v1.4/summoner/by-name/%@", region, sanitizedName];
     
     [[CRFiddleAPIClient sharedInstance] GET:url parameters:requestParams success:^(NSURLSessionDataTask *task, id responseObject) {
         NSDictionary *dict = (NSDictionary *)responseObject;
